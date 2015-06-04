4935b587a65d9c9ddc511d97fd8acee5a0162d6a
Kramboja <koen5067@icloud.com>
2015-06-04 09:51:33 +0200

    basic movement + own scene added

---
+++ Game_Lab_3-Networking/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float _Speed = 12f;
+	private float _movex = 0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_movex = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (_movex * _Speed, -_Speed);
+
+		//if(Input.GetKeyDown(KeyCode.Space))
+		//{
+		//	GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10000f);
+		//}
+	}
+}
