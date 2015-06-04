152c59f73cd4350c2e92ab550abe8f7c450e46b6
Kramboja <koen5067@icloud.com>
2015-06-04 10:24:57 +0200

    jump and double jump added

--- Game_Lab_3-Networking/Assets/PlayerMovement.cs
+++ Game_Lab_3-Networking/Assets/PlayerMovement.cs
@@ -3,21 +3,43 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	private float _Speed = 12f;
-	private float _movex = 0f;
+	private float _Speed;
+	private float _movex;
+	private bool _jumping;
+	private bool _doublejump;
 
-	// Use this for initialization
-	void Start () {
+	void Start()
+	{
+		_Speed = 12f;
+		_jumping = false;
+		_doublejump = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		_movex = Input.GetAxis ("Horizontal");
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (_movex * _Speed, -_Speed);
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (_movex * _Speed, GetComponent<Rigidbody2D>().velocity.y);
 
-		//if(Input.GetKeyDown(KeyCode.Space))
-		//{
-		//	GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10000f);
-		//}
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			if(_doublejump)
+				return;
+
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * 400f);
+
+			if(_jumping)
+				_doublejump = true;
+			else
+				_jumping = true;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag == "floor")
+		{
+			_jumping = false;
+			_doublejump = false;
+		}
 	}
 }
