084103ac1880ac47f1a58b5f56a648ad74eef568
Kramboja <koen5067@icloud.com>
2015-06-19 09:54:15 +0200

    quick multiplayer fix for the presentation

---
+++ Game_Lab_3-Networking/Assets/PlayerNetworkSetup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+	
+	// Use this for initialization
+	void Start () 
+	{
+		if(isLocalPlayer)
+		{
+			GetComponent<PlayerMovement>().enabled = true;
+		}
+	}
+}
---
+++ Game_Lab_3-Networking/Assets/PlayerSyncPosition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncPosition : NetworkBehaviour {
+	
+	[SyncVar]
+	private Vector3 _syncPos;
+	
+	[SerializeField] Transform myTransform;
+	[SerializeField] float lerpRate = 15;
+	
+	void FixedUpdate()
+	{
+		TransmitPosition();
+		LerpPosition();
+	}
+	
+	void LerpPosition()
+	{
+		if(!isLocalPlayer)
+		{
+			myTransform.position = Vector3.Lerp(myTransform.position,_syncPos, Time.deltaTime * lerpRate);
+		}
+	}
+	
+	[Command]
+	void CmdProvidePositionToServer(Vector3 pos)
+	{
+		_syncPos = pos;
+	}
+	
+	[ClientCallback]
+	void TransmitPosition()
+	{
+		if(isLocalPlayer)
+		{
+			CmdProvidePositionToServer(myTransform.position); 
+		}
+	}
+}
